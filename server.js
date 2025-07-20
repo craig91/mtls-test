b635287cb90571d8c6db8837ef44c8dc849fcedc
craig91 <craigdejean1@gmail.com>
2025-07-20 22:23:49 +0900

    updated server code. can connect to db. create new .env

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
 const app = express();
 const port = 3000;  
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
 app.get('/', (req, res) => {
     res.send('Testing server response');
 })
+
+
+app.get('/users', (req, res) => {
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            console.error('Error querying DB:', error);
+            return res.status(500).send('Database error');
+        }
+        res.json(results);
+    });     
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
