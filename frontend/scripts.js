8be570d5f61f98ee1a1eca8df40cb2d90cfe8e65
craig91 <craigdejean1@gmail.com>
2025-07-21 19:08:50 +0900

    console logging fetch response

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -2,6 +2,7 @@ document.getElementById('loadusersBtn').addEventListener('click', () => {
     fetch('http://192.168.1.10:3000/users')
         .then(response => {
             if(!response.ok) throw new Error("Network response was not ok");
+            console.log(response);
             return response.json();
         })
 })
